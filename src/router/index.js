713b9e76e7b63f3cdda3f23d72b91dc916911afc
liguopan1990 <35193368+liguopan1990@users.noreply.github.com>
2020-06-08 13:55:03 +0800

    Create index.js

---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import login from '@/components/login'
+import tab from '@/components/user'
+import c1 from '@/components/1'
+import c2 from '@/components/2'
+import c3 from '@/components/3'
+import c4 from '@/components/4'
+
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: login
+    },
+    {
+      path: '/tab',
+      name: 'tab',
+      component: tab,
+      redirect: {name: 'c1'},
+      children:[{
+        path: '/c1',
+        name: 'c1',
+        component: c1
+      },{
+        path: '/c2',
+        name: 'c2',
+        component: login
+      },{
+        path: '/c3',
+        name: 'c3',
+        component: c3
+      },{
+        path: '/c4',
+        name: 'c4',
+        component: c4
+      }]
+    }
+  ]
+})
