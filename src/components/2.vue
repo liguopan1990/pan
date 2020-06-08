2c81910b9946baac1ffb2632ef9f5c5333d456a3
liguopan1990 <35193368+liguopan1990@users.noreply.github.com>
2020-06-08 13:57:03 +0800

    Create 2.vue

---
+++ src/components/2.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+       2222222222222
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            selected:''    
+        }
+    },
+    
+    methods: {
+        
+    },
+
+    mounted () {
+        
+        
+    }
+} 
+</script>
+
+
+<style scope>
+    
+</style>
