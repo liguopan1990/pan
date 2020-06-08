b8c370866ae34f10c6ef6c56f0f849c68b4ef7a1
liguopan1990 <35193368+liguopan1990@users.noreply.github.com>
2020-06-08 13:57:29 +0800

    Create 4.vue

---
+++ src/components/4.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        4444444444444
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            selected:''    
+        }
+    },
+    
+    methods: {
+        
+    },
+
+    mounted () {
+        
+        
+    }
+} 
+</script>
+
+
+<style scope>
+    
+</style>
