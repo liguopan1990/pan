1f1e5ac90d0e5cc7da966437857f4f0b987a75c3
liguopan1990 <35193368+liguopan1990@users.noreply.github.com>
2020-06-08 13:57:16 +0800

    Create 3.vue

---
+++ src/components/3.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        333333333333
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            selected:''    
+        }
+    },
+    
+    methods: {
+        
+    },
+
+    mounted () {
+        
+        
+    }
+} 
+</script>
+
+
+<style scope>
+    
+</style>
