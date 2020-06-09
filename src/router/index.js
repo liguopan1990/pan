9e2c5d99e4e896461b8cc177ca684be51bb15cba
liguopan1990 <35193368+liguopan1990@users.noreply.github.com>
2020-06-09 10:43:54 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -14,12 +14,12 @@ export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '/',
+      path: '/test',
       name: 'login',
       component: login
     },
     {
-      path: '/tab',
+      path: '/',
       name: 'tab',
       component: tab,
       redirect: {name: 'c1'},
